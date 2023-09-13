50ec8b999b0d3c95802c2092543d46fbc07608a4
Sebastian <sebastian.nordby.bjornstad@wlcom.no>
2023-09-13 14:15:42 +0200

    Configuration of frontend

--- src/MiniECommerce/Frontends/miniecommerce.desktop/src/App.js
+++ src/MiniECommerce/Frontends/miniecommerce.desktop/src/App.js
@@ -1,24 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import { AuthProvider } from 'oidc-react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+const oidcConfig = {
+  onSignIn: () => {
+    // Redirect?
+  },
+  authority: 'https://localhost:5010/',
+  clientId: 'miniecommerce.desktop.react',
+  redirectUri: 'https://localhost:3000/',
+};
+
+function Home() {
+  return (
+    <h1>Hello</h1>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthProvider {...oidcConfig}>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Home/>} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
