a7820de036832ceefc6012d56aa1e552cf0b1e61
Sebastian Nordby <sebastian.nordby.bjornstad@wlcom.no>
2023-10-28 16:42:18 +0200

    ProductService: Added FindProductByIdQueryTest

---
+++ src/MiniECommerce/Services/ProductService/ProductService.Tests/Domain/UseCases/Queries/FindProductByIdQueryTest.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using ProductService.Domain.UseCases.Commands.CreateProduct;
+using ProductService.Domain.UseCases.Queries.Find;
+using ProductService.Library.Models;
+using ProductService.Tests.Domain.UseCases.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductService.Tests.Domain.UseCases.Queries
+{
+    public class FindProductByIdQueryTest : BaseProductServiceTest
+    {
+        [Test]
+        public async Task TestQuery()
+        {
+            var mediator = Services.GetService<IMediator>();
+            var productId = await mediator.Send(new CreateProductCommand(
+                new ProductDto()
+                {
+                    Number = 1,
+                    Name = nameof(CreateProductCommandTest)
+                }
+            ));
+
+            var productView = await mediator.Send(
+                new FindProductByIdQuery(productId));
+
+            Assert.IsNotNull(productView);
+            Assert.That(productId == productView.Id);
+        }
+    }
+}
---
+++ src/MiniECommerce/Services/ProductService/ProductService.Tests/Domain/UseCases/Queries/FindProductViewByIdQueryTest.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using ProductService.Domain.UseCases.Commands.CreateProduct;
+using ProductService.Domain.UseCases.Queries.Find;
+using ProductService.Domain.UseCases.Queries.List;
+using ProductService.Library.Models;
+using ProductService.Tests.Domain.UseCases.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductService.Tests.Domain.UseCases.Queries
+{
+    public class FindProductViewByIdQueryTest : BaseProductServiceTest
+    {
+        [Test]
+        public async Task TestQuery()
+        {
+            var mediator = Services.GetService<IMediator>();
+            var productId = await mediator.Send(new CreateProductCommand(
+                new ProductDto()
+                {
+                    Number = 1,
+                    Name = nameof(CreateProductCommandTest)
+                }
+            ));
+
+            var productView = await mediator.Send(
+                new FindProductViewByIdQuery(productId));
+
+            Assert.IsNotNull(productView);
+            Assert.That(productId == productView.Id);
+        }
+    }
+}
